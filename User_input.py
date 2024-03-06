0dfbe04771d59c09d8ba78798d8ff45fe82e4c97
unknown <maswilimiriam@gmail.com>
2024-03-06 10:35:58 +0300

    first commit

---
+++ User_input.py
@@ -0,0 +1,6 @@
+name = str (input ("Enter your first name:"))
+name1 = str (input ("Enter your second name :"))
+Age = int (input("Enter your age :"))
+Location= input(" What is your current location :")
+print ("Thank you "+ name +" " + name1 + " for joining us .We are always pleased to have people in " + Location + " join us .Your "+ str(Age) + " years will be of great benefit to us .")
+
