706a3fa4fff6f75c9f3a6b648cca3cc179d82dff
unknown <maswilimiriam@gmail.com>
2024-03-12 10:51:31 +0300

    Practise on python data types

---
+++ my_list.py
@@ -0,0 +1,17 @@
+
+my_list = []
+New_values=[10,20,30,40]
+my_list.extend(New_values)
+print(my_list)
+print(my_list[0])
+my_list[2]=15
+print (my_list)
+Another_list =[50,60,70]
+my_list.extend(Another_list)
+print (my_list)
+my_list.pop(-1)
+print(my_list)
+my_list.sort()
+print(my_list)
+my_list.index(30)
+print(my_list.index(30))
